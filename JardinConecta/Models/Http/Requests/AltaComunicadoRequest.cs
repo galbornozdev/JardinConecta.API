c2e759eb2786ef774bb95c361536e17dd73344e3
Gonzalo Albornoz <gonzalo.hr.albornoz@gmail.com>
2026-03-08 13:22:25 -0300

    habilitar/rechazar como feature pendiente

--- JardinConecta/Models/Http/Requests/AltaComunicadoRequest.cs
+++ JardinConecta/Models/Http/Requests/AltaComunicadoRequest.cs
@@ -8,6 +8,7 @@
         public string ContenidoTextoPlano { get; set; } = null!;
         public int Estado { get; set; } = 2; // 2 = Publicado por defecto
         public DateTime? FechaPrograma { get; set; }
+        // public bool HabilitarAprobarRechazar { get; set; } // TODO: Feature pendiente
         public List<IFormFile> Archivos { get; set; } = new List<IFormFile>();
     }
 }
